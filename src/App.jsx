6fa080cf617f9221aa3813f0672c731093edbdb9
frandar7 <francesco.dargenio@fcr.re.it>
2025-07-29 16:50:04 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Vacanzeggio 2025</h1>
+      <p>Benvenuto nella versione pubblicabile dell’app!</p>
+    </div>
+  )
+}
+
+export default App;
